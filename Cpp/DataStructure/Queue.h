8a58b77ff227b168df169df99dcd6c62357b4369
nuke1115 <108456412+nuke1115@users.noreply.github.com>
2024-05-31 23:34:20 +0900

    Add files via upload

---
+++ Cpp/DataStructure/Queue.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#ifndef QueueHeader
+#define QueueHeader
+#include "LinkedList.h"
+#include <limits>
+using namespace std;
+
+template<class T>
+class Queue
+{
+private:
+	long long _count = 0;
+	long long _maxCount = numeric_limits<long long>::max();
+	SinglyLinkedList<T> _internalList = SinglyLinkedList<T>();
+	bool _isEmpty = true;
+public:
+	Queue()
+	{}
+	~Queue()
+	{
+
+	}
+
+	void Enqueue(T item)
+	{
+		if (_count == _maxCount)
+		{
+			puts("Queue is FULL!");
+			return;
+		}
+		_internalList.AddLast(item);
+		_count++;
+		_isEmpty = false;
+	}
+
+	T Dequeue()
+	{
+		T tmp{};
+		if (_isEmpty || _count == 0)
+		{
+			puts("Queue is EMPTY!");
+			_isEmpty = true;
+			return tmp;
+		}
+		_count--;
+		return _internalList.GetAndDeleteLast();
+	}
+
+	T Peek()
+	{
+		T tmp{};
+		if (_isEmpty || _count == 0)
+		{
+			puts("Queue is EMPTY!");
+			_isEmpty = true;
+			return tmp;
+		}
+		return _internalList.GetLast();
+	}
+
+	void Clear()
+	{
+		_internalList.ClearList();
+		_count = 0;
+		_isEmpty = true;
+	}
+
+	long long GetCount()
+	{
+		return _count;
+	}
+
+	bool IsEmpty()
+	{
+		return _isEmpty;
+	}
+
+};
+
+
+#endif // !QueueHeader
+/*
+todo:
+add find function (also LinkedList.h too)
+*/
